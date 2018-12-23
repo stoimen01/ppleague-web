6213e0d2f310e271b2a899f847e7c7936296017d
stoimen01 <stoimen01@gmail.com>
2018-12-23 02:36:28 +0000

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import TopFiveTable from "./TopFiveTable";
+
+class App extends React.Component {
+    render() {
+        const players = [
+            {
+                id: "123",
+                number: "0",
+                name: "Josh",
+                wins: 10,
+                losses: 10,
+                winRate: "20%"
+            },
+            {
+                id: "1234",
+                number: "1",
+                name: "Bob",
+                wins: 10,
+                losses: 10,
+                winRate: "20%"
+            }
+        ];
+
+        return (
+          <div>
+              <TopFiveTable players={players}/>
+          </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
